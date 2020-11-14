f463e1f75e21d9aaac33294fdd4e583a15895f46
Mihai Radulescu <mihai.radulescu@nerdscomputing.com>
2020-11-14 04:31:55 +0200

    added postgres configuration for dev with config factory.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import database from '../config/postgres';
 import app from '../config/app';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [database, app],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('database.host'),
+        port: +config.get<number>('database.port'),
+        username: config.get('database.user'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
-
   controllers: [AppController],
   providers: [AppService],
 })
